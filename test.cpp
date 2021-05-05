4f3cf6f0dc37e96ef9334c6cb119f434fd62e575
Yoc Lu <searcher408@gmail.com>
2021-05-05 17:37:39 +0800

    Add some files.

---
+++ test.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "Just a test." << endl;
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    system("pause");
+    return 0;
+}
